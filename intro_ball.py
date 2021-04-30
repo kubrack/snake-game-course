6ce8d574c6514799f14291f82ce2b84bd66ea3b8
Oleksandr Kubrak <kubrack@gmail.com>
2021-04-30 18:17:19 +0300

    infinity snake

--- intro_ball.py
+++ intro_ball.py
@@ -2,14 +2,10 @@ import sys, pygame, time
 pygame.init()
 
 size = width, height = 320, 240
-speed = [1, 0]
+head_pos = [ int(width/2), int(height/2) ]
 black = 0, 0, 0
 length = 1
-
-screen = pygame.display.set_mode(size)
-
-ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
+delay = 0.1
 
 # TODO:
 # - rabbits subset
@@ -18,33 +14,43 @@ ballrect = ball.get_rect()
 # - tail eating event
 # - wall hit event
 
+img = pygame.image.load("intro_ball.gif")
+img_w = img.get_rect().width
+img_h = img.get_rect().height
+
+def new_rect(where):
+    rect = img.get_rect()
+    rect.x = where[0]
+    rect.y = where[1]
+    return rect
+
+snake = [ new_rect( head_pos ) ]
+screen = pygame.display.set_mode(size)
+
 while 1:
-    time.sleep(0.001)
+    time.sleep(delay)
     for event in pygame.event.get():
       if event.type == pygame.KEYDOWN:
           if event.key == pygame.K_RIGHT:
-              speed[0] = 1
-              speed[1] = 0
+              head_pos[0] += img_w
           elif event.key == pygame.K_LEFT:
-              speed[0] = -1
-              speed[1] = 0
+              head_pos[0] -= img_w
           elif event.key == pygame.K_UP:
-              speed[1] = -1
-              speed[0] = 0
+              head_pos[1] -= img_h
           elif event.key == pygame.K_DOWN:
-              speed[1] = 1
-              speed[0] = 0
+              head_pos[1] += img_h
           elif event.key == pygame.K_ESCAPE or event.key == ord('q'):
               pygame.quit()
               sys.exit()
       if event.type == pygame.QUIT: sys.exit()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+    snake.append( new_rect( head_pos ) )
+
+    if head_pos[0] < 0 or head_pos[0] > width or head_pos[1] < 0 or head_pos[1] > height:
+        head_pos = [ int(width/2), int(height/2) ]
+        snake = [ new_rect( head_pos ) ]
 
     screen.fill(black)
-    screen.blit(ball, ballrect)
+    for r in snake:
+        screen.blit(img, r)
     pygame.display.flip()
