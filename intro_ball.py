7e6a9dd46e2aa5616a006d54ede9cf38866bca76
Oleksandr Kubrak <kubrack@gmail.com>
2021-05-01 03:24:46 +0300

    snake without rabbit

--- intro_ball.py
+++ intro_ball.py
@@ -1,4 +1,4 @@
-import sys, pygame, time
+import sys, pygame, time, random
 pygame.init()
 
 size = width, height = 320, 240
@@ -17,6 +17,7 @@ delay = 0.1
 img = pygame.image.load("intro_ball.gif")
 img_w = img.get_rect().width
 img_h = img.get_rect().height
+direction = [img_w, 0]
 
 def new_rect(where):
     rect = img.get_rect()
@@ -24,7 +25,15 @@ def new_rect(where):
     rect.y = where[1]
     return rect
 
-snake = [ new_rect( head_pos ) ]
+def init_snake():
+    head_pos[0] = int(width/2)
+    head_pos[1] = int(height/2)
+    direction[0] = img_w
+    direction[1] = 0
+    return [ new_rect( head_pos ), new_rect( [head_pos[0]+img_w, head_pos[1]] ), new_rect( [head_pos[0]+img_w*2, head_pos[1] ] ), new_rect( [head_pos[0]+img_w*3, head_pos[1] ] ) ]
+
+snake = init_snake()
+rabbit = new_rect( head_pos )
 screen = pygame.display.set_mode(size)
 
 while 1:
@@ -32,23 +41,27 @@ while 1:
     for event in pygame.event.get():
       if event.type == pygame.KEYDOWN:
           if event.key == pygame.K_RIGHT:
-              head_pos[0] += img_w
+              direction = [img_w, 0]
           elif event.key == pygame.K_LEFT:
-              head_pos[0] -= img_w
+              direction = [-img_w, 0]
           elif event.key == pygame.K_UP:
-              head_pos[1] -= img_h
+              direction = [0, -img_h]
           elif event.key == pygame.K_DOWN:
-              head_pos[1] += img_h
+              direction = [0, img_h]
           elif event.key == pygame.K_ESCAPE or event.key == ord('q'):
               pygame.quit()
               sys.exit()
       if event.type == pygame.QUIT: sys.exit()
 
-    snake.append( new_rect( head_pos ) )
+    head_pos[0] += direction[0];
+    head_pos[1] += direction[1];
+    snake.insert( 0, new_rect(head_pos) )
+    snake.pop()
+    print(head_pos);
 
+    # wall hit
     if head_pos[0] < 0 or head_pos[0] > width or head_pos[1] < 0 or head_pos[1] > height:
-        head_pos = [ int(width/2), int(height/2) ]
-        snake = [ new_rect( head_pos ) ]
+        snake = init_snake()
 
     screen.fill(black)
     for r in snake:
