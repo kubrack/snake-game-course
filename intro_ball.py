7310a97e1916d1e6f654a6a4aab70ead47ba2314
Oleksandr Kubrak <kubrack@gmail.com>
2021-05-01 10:15:54 +0300

    rabbit added

--- intro_ball.py
+++ intro_ball.py
@@ -8,11 +8,11 @@ length = 1
 delay = 0.1
 
 # TODO:
-# - rabbits subset
-# - snake growing
-# - rabbit eating event
+# + rabbits subset
+# + snake growing
+# + rabbit eating event
 # - tail eating event
-# - wall hit event
+# + wall hit event
 
 img = pygame.image.load("intro_ball.gif")
 img_w = img.get_rect().width
@@ -30,10 +30,25 @@ def init_snake():
     head_pos[1] = int(height/2)
     direction[0] = img_w
     direction[1] = 0
-    return [ new_rect( head_pos ), new_rect( [head_pos[0]+img_w, head_pos[1]] ), new_rect( [head_pos[0]+img_w*2, head_pos[1] ] ), new_rect( [head_pos[0]+img_w*3, head_pos[1] ] ) ]
+    return [ new_rect( head_pos ) ]
+
+class Rabbit:
+    def __init__(self):
+        self.respawn()
+
+    def respawn(self):
+        self.x = ( random.randint(1, width - img_w) // 8 ) * 8
+        self.y = ( random.randint(1, height - img_h) // 8 ) * 8
+        self.rect = new_rect([self.x, self.y])
+        
+    def eaten(self, snake_head):
+        print(self.rect.left, snake_head.x, self.rect.right, "---", self.rect.top, snake_head.y, self.rect.bottom)
+        if self.rect.left <= snake_head.x and self.rect.right >= snake_head.x and self.rect.top <= snake_head.y and self.rect.bottom >= snake_head.y:
+            print("eaten")
+            return 1
 
 snake = init_snake()
-rabbit = new_rect( head_pos )
+rabbit = Rabbit()
 screen = pygame.display.set_mode(size)
 
 while 1:
@@ -56,14 +71,18 @@ while 1:
     head_pos[0] += direction[0];
     head_pos[1] += direction[1];
     snake.insert( 0, new_rect(head_pos) )
-    snake.pop()
-    print(head_pos);
+    if rabbit.eaten(snake[0]):
+        rabbit.respawn()
+    else:
+        snake.pop()
 
     # wall hit
     if head_pos[0] < 0 or head_pos[0] > width or head_pos[1] < 0 or head_pos[1] > height:
         snake = init_snake()
 
     screen.fill(black)
+    screen.blit(img, rabbit.rect)
     for r in snake:
         screen.blit(img, r)
     pygame.display.flip()
+
